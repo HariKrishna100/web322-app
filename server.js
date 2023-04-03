59d6483e3774921facb12500e1ec57450e20f462
Harikrishna Paresh Patel <hpatel296@myseneca.ca>
2023-04-03 14:24:10 -0400

    Authentication

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /*********************************************************************************
- *  WEB322 – Assignment 05
+ *  WEB322 – Assignment 06
  *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part
  *  of this assignment has been copied manually or electronically from any other source
  *  (including 3rd party web sites) or distributed to other students.
@@ -19,6 +19,7 @@ const streamifier = require("streamifier");
 const exphbs = require("express-handlebars");
 const stripJs = require("strip-js");
 const blogData = require("./blog-service.js");
+const authData = require("auth-service.js");
 const path = require("path");
 const app = express();
 const upload = multer();
